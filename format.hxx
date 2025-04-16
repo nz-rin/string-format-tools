2d8a3207174b3db510193ad4667d5e3ea509396d
NZ <-->
2025-04-16 14:17:14 +0200

    Cleaned main and made it easier to read and removed unused <cstring> header.

--- format.hxx
+++ format.hxx
@@ -1,5 +1,4 @@
 
-#include <cstring>
 #include <string>
 
 namespace pad{
--- main.cxx
+++ main.cxx
@@ -4,9 +4,10 @@
 
 int main(){
 	std::string  str = "Hello world";
+	std::string  end = "Run successfull";
 
-	pad::left(str, ' ', strlen("Run successfull"));
+	pad::left(str, ' ', end.size());
 	std::cout << format::format_string(str, {format::underline, format::fg_yellow}) << format::clear<< '\n';
-	std::cout << format::bold << format::fg_green << "Run successfull" << format::clear << std::endl;
+	std::cout << format::bold << format::fg_green << end << format::clear << std::endl;
 	return 0;
 }
